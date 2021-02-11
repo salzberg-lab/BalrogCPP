83287edc19c1dcf6f15957ef6278a9cc39c904c6
markus <markusjsommer@gmail.com>
2021-02-11 14:44:42 -0500

    add exit codes

--- main.cpp
+++ main.cpp
@@ -34,20 +34,20 @@ int main(int argc, char* argv[]) {
     // check validity and display help
     if (result.count("help") or not result.count("in")){
         std::cout << options.help() << std::endl;
-        exit(0);
+        return 1;
     }
 
     // check input and output paths
     if (not result.count("in") or not result.count("out")){
         std::cout << "Please specify input and output paths" << std::endl;
-        exit(0);
+        return 1;
     }
 
     // check translation table
     int table = result["table"].as<int>();
     if (table != 11 and table != 4){
         std::cout << "Only translation tables 11 and 4 are currently implemented. Please open a GitHub issue if you need another." << std::endl;
-        exit(0);
+        return 1;
     }
 
 
